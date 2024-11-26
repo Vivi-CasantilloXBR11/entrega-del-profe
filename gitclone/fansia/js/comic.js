84a2e9fb72e475720b00fbfe137ce66cbd2e403d
Vivi-CasantilloXBR11 <viviana.castillo.s.clemholg@gmail.com>
2024-11-25 23:59:04 -0500

    Add files via upload
    Entrega Profe

---
+++ gitclone/fansia/js/comic.js
@@ -0,0 +1,25 @@
+//array de objetos
+
+let capitulos = [
+    {
+        cap: 1,
+        habilitado: true,
+        currentScene: 1,
+        portada: "../assets/multimedia/cap1.png",
+        webPage: "../pages/capitulo1.html"
+    },
+    {
+        cap: 2,
+        habilitado: false,
+        currentScene: 1,
+        portada: "",
+        webPage: "../pages/capitulo2.html"
+    },
+    {
+        cap: 3,
+        habilitado: false,
+        currentScene: 1,
+        portada: "",
+        webPage: "../pages/capitulo3.html"
+    }
+]
